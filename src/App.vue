b7070b9f462668354927937c6add6f04fcd2c495
fcu-d0381721 <s292423@gmail.com>
2019-08-16 16:19:39 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ data }}</h1>
+    <test></test>
+    <ve-line v-bind:data="chartData" :settings="chartSettings"></ve-line>
+    <div class="btn" v-on:click="changeData">改變資料</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import test from './components/test'
+import VeLine from 'v-charts/lib/line.common'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      data: "hihi",
+      chartData: {
+          columns: ['日期', '访问用户', '下单用户', '下单率'],
+          rows: [
+            { '日期': '1/1', '访问用户': 1393, '下单用户': 1093, '下单率': 0.32 },
+            { '日期': '1/2', '访问用户': 3530, '下单用户': 3230, '下单率': 0.26 },
+            { '日期': '1/3', '访问用户': 2923, '下单用户': 2623, '下单率': 0.76 },
+            { '日期': '1/4', '访问用户': 1723, '下单用户': 1423, '下单率': 0.49 },
+            { '日期': '1/5', '访问用户': 3792, '下单用户': 3492, '下单率': 0.323 },
+            { '日期': '1/6', '访问用户': 4593, '下单用户': 4293, '下单率': 0.78 }
+          ]
+        }
+    }
+  },components: {
+    test,VeLine
+  },methods: {
+    changeData: function(){
+      this.chartData.rows[1].下单率 += 100
+    }
   }
 }
 </script>
@@ -24,5 +48,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btn {
+  cursor: pointer;
 }
 </style>
